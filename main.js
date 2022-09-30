9e36d47df7c6b6961f8f96078e8510ebd947167c
Domains18 <gibsonsgibson88@gmail.com>
2022-09-30 12:44:41 +0300

    js updates

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
 var filter = document.getElementById('filter');
+let deleteMe = document.querySelectorAll('ul');
 //form subimt event
 form.addEventListener('submit', addItem);
 
@@ -9,53 +10,39 @@ itemList.addEventListener('click', removeItem);
 
 //filter event
 filter.addEventListener('keyup', filterItems);
-
 //add item function
 function addItem(e) {
-   e.preventDefault();
-   
-   //new input value
-   var newItem = document.getElementById('item').value;
+e.preventDefault();
 
-   //creating new list elements
-   var li = document.createElement('li');
-
-   //adding the class
-   li.className = 'list-group-item';
-   //adding a text node with the input value
-   li.appendChild(document.createTextNode(newItem));
-
-   //creating the delete button element
-   var deleteBtn = document.createElement('button');
-
-   //adding classes to  deleteBtn
-   deleteBtn.className = 'fa-solid fa-square-xmark delete';
-   //appending the textnode
-   deleteBtn.appendChild(document.createTextNode('X'));
-
-   //appending the button to the li
-  li.appendChild(deleteBtn);
-
-   //appending li to list
-   itemList.appendChild(li);
-
-
+//new input value
+var newItem = document.getElementById('item').value;
+//creating new list elements
+var li = document.createElement('li');
+//adding the class
+li.className = 'list-group-item';
+//adding a text node with the input value
+li.appendChild(document.createTextNode(newItem));
+//creating the delete button element
+    var deleteBtn = document.createElement('i');
+//adding classes to  deleteBtn
+    deleteBtn.className = 'uis uis-times-circle';
+    li.appendChild(deleteBtn)
+//appending the button to the li
+    li.appendChild(deleteBtn);
+    //appending li to list
+    itemList.appendChild(li);
 }
 
-//removing item
-function removeItem(e) {
-    if(e.target.classlist.contains('delete')) {
-        if(confirm('Delete forever')) {
-            var li = e.target.parentElement;
-            itemList.removeChild(li);
-        }
+function removeItem(e){
+    if (e.target.className == "uis uis-times-circle"){
+        console.log("data");
+        let ul = document.getElementById('items');
+        let li =document.querySelector('li');
+        ul.removeChild(li)
     }
-
 }
-
 //filter items
 function filterItems(e) {
-
     //changing character to lowercase
     var text = e.target.value.toLowerCase();
 
@@ -72,6 +59,6 @@ function filterItems(e) {
         }
     })
 
-   
+
     
 }
